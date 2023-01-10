1a6a4a624462e4f1c5642c5d55ab73407f12ea60
Liz Kaufman <lizkaufman92@gmail.com>
2023-01-10 00:31:49 +0000

    added header for CORS - take two!

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,19 @@ import { getAnswer } from "./controller";
 const PORT: number = 3000;
 
 const server = http.createServer((req, res) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, GET",
+  };
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+
   if (req.method === "GET") {
-    res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+    res.writeHead(200, headers);
     return getAnswer(req, res);
   }
 });
