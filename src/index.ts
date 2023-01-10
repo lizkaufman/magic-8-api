1f3458ca019210aaa47707cb56e05a864adb7e7a
Liz Kaufman <lizkaufman92@gmail.com>
2023-01-10 00:25:46 +0000

    added header for CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import http from "http";
-import generateRandomAnswer from "../answers";
 import { getAnswer } from "./controller";
 
 const PORT: number = 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/api") {
+  if (req.method === "GET") {
+    res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
     return getAnswer(req, res);
   }
 });
